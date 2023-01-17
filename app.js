c78ecb073602e4bfd54e1331651c2c90d35c3f78
LautaroRobles05 <lautaro.robles1992@gmail.com>
2023-01-17 19:37:26 -0300

    agregando script y configurando PORT

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
@@ -9,13 +9,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve("./views/home.html"));
 });
 
-// app.get('/', (req,res)=>{
-//     res.sendFile(__dirname + '/views/home.html');
-// });
 
-// app.get('/register', (req,res)=>{
-//     res.sendFile(__dirname + '/views/register.html');
-// });
 
 app.get("/register", (req, res) => {
   res.sendFile(path.resolve("./views/register.html"));
